91ccaf8f2c7cb670a7eba4b8e9033efa616f1225
AyadAbdullah <158589968+AyadAbdullah@users.noreply.github.com>
2024-03-28 15:02:47 +0000

    Finished the map in class function

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 class Hitman2Game:
     def __init__(self):
         self.rooms = [
-            
-        ]
+        [{"description":"Miami - Hawke's Bay:",
+         "interactables":["Beach Chair", "Surfboard"]}],
+        [{"description":"Hawke's Bay Safe House",
+         "interactables":["Safe","Computer"]}],
+        [{"description": "Hawke's Bay Beach",
+         "interactables":["Fishing Rod","Shovel"]}],
+        [{"description":"Miami - The Finish Line",
+         "interactables":["Race Car", "Pit Crew"]}],
+        [{"description":"Miami - Miami Beach",
+        "interactables":["Beach Ball", "Beach Umbrella"]}],
+        [{"description":"Miami - Kronstadt Industries",
+        "interactables":["Security Camera","Keycard"]}],
+        [{"description":"Miami - City Center",
+        "interactables":["Crowd","Street Vendor"]}],
+        [{"description":"Miami - Marquez Family Mansion",
+        "interactables":["Guard","Lawn Mower"]}],
+        [{"description":"Android Soldier Testing Room",
+        "interactables":"Rober Knox"}]
+        }
