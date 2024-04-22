6c5d12c9919f2d272b0fc577df36492490345d2a
AyadAbdullah <158589968+AyadAbdullah@users.noreply.github.com>
2024-04-22 06:22:36 +0000

    Start of Version 2(All pushes onwards will be V2 until V3)

--- main.py
+++ main.py
@@ -12,34 +12,42 @@
 #######################################################################################
 
 #IMPORTS AND GLOBAL VARIABLES-----------------------------------------------------------
-current_position = (0, 0)
-rooms = {
-    (0, 0): {
-        "description": "Miami - Hawke's Bay, New Zealand"
-    },
-    (1, 0): {
-        "description": "Hawke's Bay Safe House"
-    },
-    (1, 1): {
-        "description": "Hawke's Bay Beach"
-    },
-    (2, 0): {
-        "description": "Miami - The Finish Line"
-    },
-    (3, 0): {
-        "description": "Miami - Miami Beach"
-    },
-    (3, 1): {
-        "description": "Miami - Kronstadt Industries"
-    },
-    (4, 0): {
-        "description": "Miami - City Center"
-    },
-    (4, 1): {
-        "description": "Miami - Marquez Family Mansion"
-    },
-}
+from typing import Dict, Tuple
 
+from tabulate import tabulate
+
+current_position = (0, 0)
+
+rooms : Dict[Tuple[int, int], str] = {
+    (0, 0): "Miami - Hawke's Bay, New Zealand",
+    (0, 1): "Hawke's Bay Beach",
+    (0, 2): "Miami Beach",
+    (0, 3): "Security Room",
+    (1, 0): "Hawke's Bay Safe House",
+    (1, 1): "Miami - City Center",
+    (1, 2): "Hotel & Expo Room",
+    (1, 3): "Miami - Kronstadt Industries",
+    (2, 0): "Miami - Marquez Family Mansion",
+    (2, 1): "Miami - VIP Area",
+    (2, 2): "Miami - The Finish Line",
+    (2, 3): "Miami - Android Soldier Testing Room",
+}
+#This will determine the number of rows and columns in map.txt
+num_rows = 4
+num_cols = 3
+
+map_table_data = []
+for row in range(num_rows):
+    map_rows = []
+    for col in range(num_cols):
+        room_name = rooms.get((row, col), "")
+        map_rows.append(room_name)
+    map_table_data.append(map_rows)
+
+map_table = tabulate(map_table_data, tablefmt = "grid")
+
+with open("map.txt", "w") as file:
+    file.write(map_table)
 
 #FUNCTIONS------------------------------------------------------------------------------
 def display_room_description(current_position):
@@ -79,7 +87,7 @@ def start_game():
     print("Welcome to Hitman 2: The Text Adventure!")
     print("You are Agent 47, a highly skilled assasin")
     print("Your mission is to eliminate your targets without getting caught")
-    print("Be cautios and plan your moves carefully")
+    print("Be cautious and plan your moves carefully")
     while True:
         display_room_description(current_position)
         print("What do you want to do?")
